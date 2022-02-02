4404a361f3ae32ad60db57a554865566e254a086
SojaMilchh <hlrockwelljones@gmail.com>
2022-02-02 12:09:13 +0000

    38:00- Added ability to shoot projectiles cleanly

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ class Player {
     
 }
 
+// object for bullets
 class Projectile {
     constructor(x, y, radius, color, velocity){
         this.x = x
@@ -42,13 +43,19 @@ class Projectile {
         this.color = color
         this.velocity = velocity
     }
+    //Draws the circle for the bullet
     draw(){
         canvasContext.beginPath()
         canvasContext.arc(this.x,this.y,this.radius,0,Math.PI*2, false)
         canvasContext.fillStyle = this.color
         canvasContext.fill()
     }
-
+    //Animates the movement of the bullet
+    update(){
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
 }
 
 const middleX = canvas.width / 2
@@ -59,20 +66,53 @@ const player = new Player(middleX,middleY, 30, 'red')
 player.draw()
 
 
+
+const projectile = new Projectile(
+    canvas.width / 2, //Projectile needs to spawn from centre of player
+    canvas.height / 2, 
+    5,
+    'blue', 
+    //this is a nested object (velocities properties)
+    {
+        x: 1,
+        y: 1
+    }
+)
+
+//used to manage different instances of the same object
+const projectiles = []
+
+//creates a function that updates every frame automatically
+function animate(){
+    requestAnimationFrame(animate)
+    canvasContext.clearRect(0,0,canvas.width, canvas.height)
+    player.draw()
+    projectiles.forEach(projectile => {projectile.update()})
+}
+
 //Listens for new events in the eventloop
 addEventListener('click', 
     (event) => 
     {
-        const projectile = new Projectile(
-            event.clientX, 
-            event.clientY, 
-            5, 
-            'blue', 
-            null
+        const angle = Math.atan2(
+            event.clientY - canvas.height / 2, 
+            event.clientX - canvas.width / 2
         )
-        projectile.draw()
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+        projectiles.push(
+            new Projectile(
+                canvas.width / 2,
+                canvas.height / 2,
+                2,
+                'blue',
+                velocity
+                )
+        )
     }
 )
 
 
-console.log(player)
+animate()
